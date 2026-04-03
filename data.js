038c90043d9a73e2a70a94b38ee80f12864ced8c
chrishanpeiris <chrishanmx@gmail.com>
2026-04-03 11:44:43 +1100

    Initial HTML structure, styles and data

---
+++ data.js
@@ -0,0 +1,48 @@
+const CATEGORIES = [
+  { name: "technology", color: "#93c5fd" }, // lighter blue
+  { name: "science", color: "#86efac" }, // lighter green
+  { name: "finance", color: "#fca5a5" }, // lighter red
+  { name: "society", color: "#fde047" }, // lighter yellow
+  { name: "entertainment", color: "#f9a8d4" }, // lighter pink
+  { name: "health", color: "#5eead4" }, // lighter teal
+  { name: "history", color: "#fdba74" }, // lighter orange
+  { name: "news", color: "#c4b5fd" }, // lighter purple
+];
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+// LINK TO APP SAMPLE DATA: https://docs.google.com/spreadsheets/d/1eeldcA_OwP4DHYEvjG0kDe0cRys-cDPhc_E9P9G1e3I/edit#gid=0
+
+// 👍 🤯 ⛔️
