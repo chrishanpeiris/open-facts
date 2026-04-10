d5ce90b35b9d007327590aa189a070c0bc1135fe
chrishanpeiris <chrishanmx@gmail.com>
2026-04-10 17:11:16 +1000

    created category and facts list components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,50 @@
 import "./style.css";
+
+const CATEGORIES = [
+  { name: "technology", color: "#93c5fd" }, // lighter blue
+  { name: "science", color: "#86efac" }, // lighter green
+  { name: "finance", color: "#fca5a5" }, // lighter red
+  { name: "society", color: "#fde047" }, // lighter yellow
+  { name: "entertainment", color: "#f9a8d4" }, // lighter pink
+  { name: "health", color: "#5eead4" }, // lighter teal
+  { name: "history", color: "#fdba74" }, // lighter orange
+  { name: "news", color: "#c4b5fd" }, // lighter purple
+];
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
 function App() {
   const appTitle = "Open Facts";
 
@@ -10,7 +56,7 @@ function App() {
           <img src="logo.png" alt="Open Facts Logo" />
           <h1>{appTitle}</h1>
         </div>
-        <button class="btn btn-large btn-open">Share a fact</button>
+        <button className="btn btn-large btn-open">Share a fact</button>
       </header>
       <NewFactForm />
       <main className="main">
@@ -26,11 +72,73 @@ function NewFactForm() {
 }
 
 function CategoryFilter() {
-  return <aside>Category filter</aside>;
+  return (
+    <aside>
+      <ul>
+        <li className="category">
+          <button className="btn btn-all-categories">All</button>
+        </li>
+        {CATEGORIES.map((cat) => (
+          <li key={cat.name} className="category">
+            <button
+              className="btn btn-category"
+              style={{ backgroundColor: cat.color }}
+            >
+              {cat.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 function FactList() {
-  return <section>Fact List</section>;
+  // Temporary
+  const facts = initialFacts;
+  return (
+    <section>
+      <ul className="facts-list">
+        {facts.map((fact) => (
+          <Fact key={fact.id} fact={fact} />
+        ))}
+      </ul>
+      <p>There are {facts.length} in the database</p>
+    </section>
+  );
+}
+
+function Fact({ fact }) {
+  return (
+    <li className="fact">
+      <p>
+        {fact.text}
+        <a className="source" href={fact.source} target="_blank">
+          (Source)
+        </a>
+      </p>
+      <span
+        className="tag"
+        style={{
+          backgroundColor: CATEGORIES.find((cat) => cat.name === fact.category)
+            ?.color,
+        }}
+      >
+        {fact.category}
+      </span>
+      <div className="vote-buttons">
+        <button>
+          👍 <strong>{fact.votesInteresting} </strong>
+        </button>
+        <button>
+          🤯 <strong>{fact.votesMindblowing} </strong>
+        </button>
+        <button>
+          ⛔️ <strong>{fact.votesFalse} </strong>
+        </button>
+      </div>
+    </li>
+  );
 }
 
 export default App;
